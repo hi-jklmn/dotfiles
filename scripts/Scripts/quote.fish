cb4e34b524222e145920b03af737cb83b638d180
jokulhaups <jokullmani12@gmail.com>
2022-03-30 04:10:38 +0000

    Add ability to print quote by index

--- scripts/Scripts/quote.fish
+++ scripts/Scripts/quote.fish
@@ -6,7 +6,17 @@ set TEMP /tmp/quotes.json
 yq -o=json . $FILE > $TEMP
 
 set LENGTH (cat $TEMP | jshon -e quotes -l)
-set QUOTE  (cat $TEMP | jshon -e quotes -e (math (date +%N) % $LENGTH))
+
+if test (count $argv) -eq 1
+    ; and isnumber.fish $argv[1]
+    ; and test $argv[1] -lt $LENGTH
+    ; and test $argv[1] -ge 0
+    set INDEX $argv[1]
+else
+    set INDEX  (math (date +%N) % $LENGTH)
+end
+
+set QUOTE  (cat $TEMP | jshon -e quotes -e $INDEX)
 
 echo " "
 echo $QUOTE | jshon -e quote -u | fold -w 80 -s - | awk '{ print "\t" $0 }'
