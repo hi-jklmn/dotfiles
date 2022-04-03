37d1a0dcc62c860a0c42eac6573609174e364c64
jokulhaups <jokullmani12@gmail.com>
2022-04-03 17:55:22 +0000

    Fix typo

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,7 @@
 local confdir = vim.fn.stdpath('config')
 
-vim.cmd('source ' .. confdir .. 'setup.vim')
-vim.cmd('source ' .. confdir .. 'keybinds.vim')
+vim.cmd('source ' .. confdir .. '/setup.vim')
+vim.cmd('source ' .. confdir .. '/keybinds.vim')
 require('plugins')
 
 print 'init.lua loaded'
